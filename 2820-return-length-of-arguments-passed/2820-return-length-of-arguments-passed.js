9d6c8e2904d9a7bcf3d65eeb70c42c9d38d16fa2
karan der <126866154+KARANDER@users.noreply.github.com>
2025-01-13 12:46:22 +0530

    Time: 52 ms (53.57%), Space: 48.4 MB (92.88%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,21 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+   let i;
+   if(args.length=== 0){
+   return 0;
+   }
+    else {
+     for( i in args ){
+         i++;
+   } 
+     return i;
+    }
+   
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
