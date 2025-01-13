5747efad8782f854f1dbd3853b363240ac5615e8
karan der <126866154+KARANDER@users.noreply.github.com>
2025-01-13 12:47:29 +0530

    Time: 33 ms (99.81%), Space: 48.7 MB (79.48%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-   let i;
-   if(args.length=== 0){
-   return 0;
-   }
-    else {
-     for( i in args ){
-         i++;
-   } 
-     return i;
-    }
+//    let i;
+//    if(args.length=== 0){
+//    return 0;
+//    }
+//     else {
+//      for( i in args ){
+//          i++;
+//    } 
+//      return i;
+//     }
    
+   return args.length
 };
 
 /**
